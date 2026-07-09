4a903f522f2daf21f261d9be4ef1fd5c81e4132a
Amritha043 <amrithaa043@gmail.com>
2026-07-09 23:13:53 +0530

    completed

--- src/components/ViewBook.jsx
+++ src/components/ViewBook.jsx
@@ -17,7 +17,8 @@ const ViewBook = () => {
                                 
                                 <div class="card-body">
                                     <img src="https://i.pinimg.com/736x/fd/a0/a9/fda0a96c1288877b416f9f7618a87df2.jpg" class="card-img-top" alt="..."height="350"/>
-                                    <h5 class="card-title">Matte Lipstick</h5>
+                                    <h5 class="card-title">How to steal a heart</h5>
+                                    <p>AUTHOR:YOU</p>
                                     <p class="card-text"><strong>Price: ₹499</strong></p>
                                     <a href="#" class="btn btn-dark">Add to Cart</a>
                                 </div>
@@ -31,7 +32,8 @@ const ViewBook = () => {
                                 
                                 <div class="card-body">
                                     <img src="https://i.pinimg.com/736x/af/44/80/af4480b2d95058a9c6790b45d0e05d13.jpg" class="card-img-top" alt="..."height="350"/>
-                                    <h5 class="card-title">Vitamin C Face Wash</h5>
+                                    <h5 class="card-title">The secret garden</h5>
+                                    <p>AUTHOR:Hogson burnett</p>
                                     <p class="card-text"><strong>Price: ₹349</strong></p>
                                     <a href="#" class="btn btn-dark">Add to Cart</a>
                                 </div>
@@ -46,7 +48,8 @@ const ViewBook = () => {
                                 
                                 <div class="card-body">
                                     <img src="https://i.pinimg.com/1200x/a2/f1/ac/a2f1ac4c7001cb25f2d6fbbc33e79e48.jpg" class="card-img-top" alt="..." height="350"/>
-                                    <h5 class="card-title">Liquid Foundation</h5>
+                                    <h5 class="card-title">The Alchiemist</h5>
+                                    <p>AUTHOR: Paulo coelo</p>
                                     <p class="card-text"><strong>Price: ₹899</strong></p>
                                     <a href="#" class="btn btn-dark">Add to Cart</a>
                                 </div>
@@ -61,7 +64,8 @@ const ViewBook = () => {
                                 
                                 <div class="card-body">
                                     <img src="https://i.pinimg.com/736x/3a/14/1e/3a141ec359673feb4dad6e64d92b1e0c.jpg" class="card-img-top" alt="..." height="350"/>
-                                    <h5 class="card-title">Liquid Foundation</h5>
+                                    <h5 class="card-title">It end with us</h5>
+                                    <p>AUTHOR: Collen Hoover</p>
                                     <p class="card-text"><strong>Price: ₹899</strong></p>
                                     <a href="#" class="btn btn-dark">Add to Cart</a>
                                 </div>
@@ -78,7 +82,8 @@ const ViewBook = () => {
                                 
                                 <div class="card-body">
                                     <img src="https://i.pinimg.com/736x/79/c5/c2/79c5c2be1504e12e4a51438faa90b526.jpg" class="card-img-top" alt="..." height="350px"/>
-                                    <h5 class="card-title">Liquid Foundation</h5>
+                                    <h5 class="card-title">Verity</h5>
+                                    <p>AUTHOR: Collen Hoover</p>
                                     <p class="card-text"><strong>Price: ₹899</strong></p>
                                     <a href="#" class="btn btn-dark">Add to Cart</a>
                                 </div>
@@ -93,7 +98,8 @@ const ViewBook = () => {
                                 
                                 <div class="card-body">
                                     <img src="https://i.pinimg.com/736x/b4/c0/f6/b4c0f6ddb1a5c552d06407edc385aa36.jpg" class="card-img-top" alt="..." height="350px"/>
-                                    <h5 class="card-title">Liquid Foundation</h5>
+                                    <h5 class="card-title">The diary of young girl</h5>
+                                    <p>AUTHOR: Annie Frank</p>
                                     <p class="card-text"><strong>Price: ₹899</strong></p>
                                     <a href="#" class="btn btn-dark">Add to Cart</a>
                                 </div>
@@ -108,7 +114,8 @@ const ViewBook = () => {
                                 
                                 <div class="card-body">
                                     <img src="https://i.pinimg.com/736x/8d/b9/82/8db98244e516e9fcf830f1e39b407691.jpg" class="card-img-top" alt="..." height="350px"/>
-                                    <h5 class="card-title">Liquid Foundation</h5>
+                                    <h5 class="card-title">Harry Potter</h5>
+                                    <p>AUTHOR: J K Rowling</p>
                                     <p class="card-text"><strong>Price: ₹899</strong></p>
                                     <a href="#" class="btn btn-dark">Add to Cart</a>
                                 </div>
@@ -123,7 +130,8 @@ const ViewBook = () => {
                                 
                                 <div class="card-body">
                                     <img src="https://i.pinimg.com/1200x/1e/83/06/1e8306f539c744ea7f52b797587f728e.jpg" class="card-img-top" alt="..." height="350px"/>
-                                    <h5 class="card-title">Liquid Foundation</h5>
+                                     <h5 class="card-title">One of us is lying</h5>
+                                    <p>AUTHOR: Karen M</p>
                                     <p class="card-text"><strong>Price: ₹899</strong></p>
                                     <a href="#" class="btn btn-dark">Add to Cart</a>
                                 </div>
@@ -138,7 +146,8 @@ const ViewBook = () => {
                                 
                                 <div class="card-body">
                                     <img src="https://i.pinimg.com/1200x/44/20/d5/4420d5835efbf33771699ac4ca4b3eb4.jpg" class="card-img-top" alt="..." height="350px"/>
-                                    <h5 class="card-title">Liquid Foundation</h5>
+                                    <h5 class="card-title">Pride and pejudice</h5>
+                                    <p>AUTHOR: Jane Austin</p>
                                     <p class="card-text"><strong>Price: ₹899</strong></p>
                                     <a href="#" class="btn btn-dark">Add to Cart</a>
                                 </div>
@@ -153,7 +162,8 @@ const ViewBook = () => {
                                 
                                 <div class="card-body">
                                     <img src="https://i.pinimg.com/1200x/07/5d/68/075d6891101a4cfae6d66f1a4b6af91a.jpg" class="card-img-top" alt="..." height="350px"/>
-                                    <h5 class="card-title">Liquid Foundation</h5>
+                                    <h5 class="card-title">Life of pie</h5>
+                                    <p>AUTHOR: Yann Martel</p>
                                     <p class="card-text"><strong>Price: ₹899</strong></p>
                                     <a href="#" class="btn btn-dark">Add to Cart</a>
                                 </div>
@@ -168,7 +178,8 @@ const ViewBook = () => {
                                 
                                 <div class="card-body">
                                     <img src="https://i.pinimg.com/736x/c5/d2/1b/c5d21b7fb9ebc896cf46d5e6d67af4a7.jpg" class="card-img-top" alt="..." height="350px"/>
-                                    <h5 class="card-title">Liquid Foundation</h5>
+                                    <h5 class="card-title">The summer i turned pretty</h5>
+                                    <p>AUTHOR: Jenny haan</p>
                                     <p class="card-text"><strong>Price: ₹899</strong></p>
                                     <a href="#" class="btn btn-dark">Add to Cart</a>
                                 </div>
@@ -183,7 +194,8 @@ const ViewBook = () => {
                                 
                                 <div class="card-body">
                                     <img src="https://i.pinimg.com/736x/8e/55/52/8e555239aa9cae3c1be57eea40f7f49e.jpg" class="card-img-top" alt="..." height="350px"/>
-                                    <h5 class="card-title">Liquid Foundation</h5>
+                                    <h5 class="card-title">To kill a mocking bird</h5>
+                                    <p>AUTHOR: Harper Lee</p>
                                     <p class="card-text"><strong>Price: ₹899</strong></p>
                                     <a href="#" class="btn btn-dark">Add to Cart</a>
                                 </div>
