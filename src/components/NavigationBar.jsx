e987517e30d4f8a39209202dd22ed6669d812ccc
Amritha043 <amrithaa043@gmail.com>
2026-07-10 09:12:46 +0530

    replace a tag with link tag for easy page reloding

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,35 +1,58 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const NavigationBar = () => {
   return (
     <div>
-  <nav class="navbar navbar-expand-lg bg-body-tertiary library-navbar shadow ">
-  <div class="container-fluid">
-    <a class="navbar-brand text-light" href="#">LIBRARY</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link " href="/"><b>ADD BOOK</b></a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link " href="/view"><b>VIEW BOOK</b></a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link "  href="/delete" ><b>DELETE BOOK</b></a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link "  href="/search" ><b>SEARCH BOOK</b></a>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
+      <nav className="navbar navbar-expand-lg bg-body-tertiary library-navbar shadow">
+        <div className="container-fluid">
+          <Link className="navbar-brand text-light" to="/">
+            LIBRARY
+          </Link>
 
-    </div>
-  )
-}
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
 
-export default NavigationBar
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <Link className="nav-link" to="/">
+                  <b>ADD BOOK</b>
+                </Link>
+              </li>
+
+              <li className="nav-item">
+                <Link className="nav-link" to="/view">
+                  <b>VIEW BOOK</b>
+                </Link>
+              </li>
+
+              <li className="nav-item">
+                <Link className="nav-link" to="/delete">
+                  <b>DELETE BOOK</b>
+                </Link>
+              </li>
+
+              <li className="nav-item">
+                <Link className="nav-link" to="/search">
+                  <b>SEARCH BOOK</b>
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default NavigationBar;
